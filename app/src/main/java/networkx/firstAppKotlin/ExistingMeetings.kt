8375e122e11f84774ed495e963077123300b3972
Luizael A. Batista <lbatista@networxenterprise.com>
2023-04-03 15:50:35 +1000

    addins list

--- app/src/main/java/networkx/firstAppKotlin/ExistingMeetings.kt
+++ app/src/main/java/networkx/firstAppKotlin/ExistingMeetings.kt
@@ -1,94 +1,98 @@
 package networkx.firstAppKotlin
 
-import android.Manifest
-import android.content.pm.PackageManager
+
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import java.io.File
+import networkx.firstAppKotlin.R.id.listViewFile
+
 
 class ExistingMeetings : AppCompatActivity() {
 
-    private lateinit var meetingsRecyclerView: RecyclerView
-    private lateinit var adapter: MeetingsAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_existing_meetings)
-
+        setContentView(R.layout.fragment_existing_meetings)//main
         val documentsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-        val folderName = "CFMEU_Meetings"
-        val folder = File(documentsDirectory, folderName)
+        val arrayAdapter: ArrayAdapter<*>
+        val listView = findViewById<ListView>(R.id.listViewFile)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                Utilities.getAllFiles( documentsDirectory.toString() ))
+        listView.adapter = arrayAdapter;
 
-        meetingsRecyclerView = findViewById(R.id.meetingsRecyclerView)
-        meetingsRecyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = MeetingsAdapter(emptyArray())
-        meetingsRecyclerView.adapter = adapter
 
-        listAllMeetings(folder)
-        //listAllMeetings(documentsDirectory)
 
-        if (!folder.exists()) {
-            Log.d("ExistingMeetings", "Directory does not exist")
-            return
-        } else (Log.d ("ExistingMeetings", "Directopry exists"))
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            // Permission has already been granted
-            listAllMeetings(folder)
-        } else {
-            // Request permission
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-            Log.d("A", "App doesn't have enough permission")
-        }
+    //        val documentsDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+//        val folderName = "CFMEU_Meetings"
+//        val folder = File(documentsDirectory, folderName)
+
+//        meetingsRecyclerView = findViewById(R.id.meetingsRecyclerView)
+//        meetingsRecyclerView.layoutManager = LinearLayoutManager(this)
+//
+//        adapter = MeetingsAdapter( Utilities.getAllFiles( documentsDirectory.toString() ))
+//
+//        meetingsRecyclerView.adapter = adapter
+
+
+
+//        if (!folder.exists()) {
+//            Log.d("ExistingMeetings", "Directory does not exist")
+//            return
+//        } else (Log.d ("ExistingMeetings", "Directopry exists"))
+//
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//            // Permission has already been granted
+//            //listAllMeetings(folder)
+//        } else {
+//            // Request permission
+//            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+//            Log.d("A", "App doesn't have enough permission")
+//        }
 
 
     }
 
-    private class MeetingsAdapter(private var fileList: Array<File>?) :
-        RecyclerView.Adapter<MeetingViewHolder>() {
+//    private class MeetingsAdapter(private var fileList: List<File>?) :
+//        RecyclerView.Adapter<MeetingViewHolder>() {
+//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeetingViewHolder {
+//            val itemView = LayoutInflater.from(parent.context)
+//                .inflate(R.layout.meeting_item_layout, parent, false)
+//            return MeetingViewHolder(itemView)
+//        }
+//
+//        override fun onBindViewHolder(holder: MeetingViewHolder, position: Int) {
+//            val file = fileList!![position]
+//            holder.fileNameTextView.text = file.name
+//        }
+//
+//        override fun getItemCount(): Int = fileList?.size ?: 0
+//
+//        fun updateList(newList: List<File>) {
+//            fileList = newList
+//            notifyDataSetChanged()
+//        }
+//    }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeetingViewHolder {
-            val itemView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.meeting_item_layout, parent, false)
-            return MeetingViewHolder(itemView)
-        }
+//    private fun listAllMeetings(directory: File) {
+//        if (!directory.exists()) {
+//            Log.d("ExistingMeetings", "Directory does not exist")
+//            return
+//        }
+//        val files = directory.listFiles()
+//        Log.d("ExistingMeetings", "Files found: ${files?.size}")
+//        if (files != null) {
+//            adapter.updateList(files)
+//        }
+//    }
 
-        override fun onBindViewHolder(holder: MeetingViewHolder, position: Int) {
-            val file = fileList!![position]
-            holder.fileNameTextView.text = file.name
-        }
 
-        override fun getItemCount(): Int = fileList?.size ?: 0
 
-        fun updateList(newList: Array<File>) {
-            fileList = newList
-            notifyDataSetChanged()
-        }
-    }
+//    private class MeetingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val fileNameTextView: TextView = itemView.findViewById(R.id.fileNameTextView)
+//    }
 
-    private fun listAllMeetings(directory: File) {
-        if (!directory.exists()) {
-            Log.d("ExistingMeetings", "Directory does not exist")
-            return
-        }
-        val files = directory.listFiles()
-        Log.d("ExistingMeetings", "Files found: ${files?.size}")
-        if (files != null) {
-            adapter.updateList(files)
-        }
-    }
 
-    private class MeetingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val fileNameTextView: TextView = itemView.findViewById(R.id.fileNameTextView)
-    }
+
 }
--- app/src/main/java/networkx/firstAppKotlin/Utilities.java
+++ app/src/main/java/networkx/firstAppKotlin/Utilities.java
@@ -1,2 +1,29 @@
-package networkx.firstAppKotlin;public class Utilities {
+package networkx.firstAppKotlin;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+public class Utilities {
+    public static List<String> getAllFiles(String URI){
+        List<String> fileNames = new ArrayList<>();
+        try {
+            Path folder = Paths.get(URI, "CFMEU_Meetings/");
+            Arrays.asList( folder.toFile().listFiles() ).forEach( file ->{
+                fileNames.add(file.getName().toString());
+            });
+
+        }catch (Exception e){
+            Log.d("file-jav-err", e.getMessage());
+        }
+        return fileNames;
+    }
 }
