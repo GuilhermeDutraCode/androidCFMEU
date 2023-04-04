780684d01cd5d72599b835211abbe68c62b96b81
Guilherme Dutra <gdutra@networxenterprise.com>
2023-04-04 13:05:23 +1000

    added calendar input and fixed meeting name, fixed the exisingMeeting.xml, added functionality to delete meetings

--- app/src/main/java/networkx/firstAppKotlin/ExistingMeetings.kt
+++ app/src/main/java/networkx/firstAppKotlin/ExistingMeetings.kt
@@ -1,38 +1,62 @@
 package networkx.firstAppKotlin
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import java.io.File
 
 
 class ExistingMeetings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_existing_meetings)//main
+        setContentView(R.layout.fragment_existing_meetings)
         val documentsDirectory =
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
         val arrayAdapter: ArrayAdapter<*>
         val listView = findViewById<ListView>(R.id.listViewFile)
-
-
 
         arrayAdapter = ArrayAdapter(
             this, android.R.layout.simple_list_item_1,
             Utilities.getAllFiles(documentsDirectory.toString())
         )
 
-        listView.adapter = arrayAdapter;
+        listView.adapter = arrayAdapter
 
-
-
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val fileToDelete = File(
+                documentsDirectory.absolutePath + "/CFMEU_Meetings",
+                arrayAdapter.getItem(position)!!
+            )
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Delete Meeting")
+            builder.setMessage("Are you sure you want to delete this meeting?")
+            builder.setPositiveButton("Yes") { _, _ ->
+                if (fileToDelete.delete()) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Meeting deleted successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    arrayAdapter.remove(arrayAdapter.getItem(position))
+                    arrayAdapter.notifyDataSetChanged()
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Unable to delete meeting",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            builder.setNegativeButton("No") { _, _ -> }
+            builder.show()
+        }
     }
-
-
 }
--- app/src/main/java/networkx/firstAppKotlin/NewMeeting.java
+++ app/src/main/java/networkx/firstAppKotlin/NewMeeting.java
@@ -2,10 +2,20 @@ package networkx.firstAppKotlin;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +29,8 @@ public class NewMeeting extends AppCompatActivity {
 
     private Button createWorkbookButton;
     private File directory;
+    private EditText dateEdt;
+    private EditText fileNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,48 @@ public class NewMeeting extends AppCompatActivity {
         setContentView(R.layout.fragment_new_meeting);
 
         createWorkbookButton = findViewById(R.id.saveA);
+        dateEdt = findViewById(R.id.idEdtDate);
+        fileNameEditText = findViewById(R.id.filename_input);
+
+        fileNameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    fileNameEditText.setText("");
+                } else {
+                    // Hide the keyboard when the editText loses focus
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+            }
+        });
+
+
+
+        //Function to make calendar pop to user pick meeting date;
+        dateEdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        NewMeeting.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dateEdt.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
+        //Function to create a new workbook;
         createWorkbookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,7 +109,7 @@ public class NewMeeting extends AppCompatActivity {
         Row row7 = sheet.createRow(6);
         Row row8 = sheet.createRow(7);
 
-        createCell(row1, 0, "Name");
+        createCell(row1, 0, "d");
         createCell(row1, 1, "Score");
 
         createCell(row2, 0, "Mike");
@@ -95,23 +149,40 @@ public class NewMeeting extends AppCompatActivity {
         }
 
         EditText fileNameEditText = findViewById(R.id.filename_input);
-        String fileName = fileNameEditText.getText().toString().trim() + ".xlsx";
+        String fileNameInput = fileNameEditText.getText().toString().trim();
 
-        if (fileName.isEmpty()) {
+        if (fileNameInput.isEmpty()) {
             Toast.makeText(this, "Please enter a file name", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        File excelFile = new File(folderPath, fileName);
+        EditText dateEdt = findViewById(R.id.idEdtDate);
+        String dateStr = dateEdt.getText().toString().trim();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+
 
         try {
-            FileOutputStream fileOut = new FileOutputStream(excelFile);
-            ourWorkbook.write(fileOut);
-            fileOut.close();
-            Toast.makeText(this, "meeting created", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
+            Date date = sdf.parse(dateStr);
+            sdf.applyPattern("yyyy-MM-dd");
+            String formattedDate = sdf.format(date);
+            String fileName = formattedDate + "__" + fileNameInput + ".xlsx";
+
+            File excelFile = new File(folderPath, fileName);
+
+            try {
+                FileOutputStream fileOut = new FileOutputStream(excelFile);
+                ourWorkbook.write(fileOut);
+                fileOut.close();
+                Toast.makeText(this, "Meeting created", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to create file", Toast.LENGTH_SHORT).show();
+            }
+        } catch (ParseException e) {
             e.printStackTrace();
-            Toast.makeText(this, "Failed to create file", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Invalid date format", Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
